18d022f326fe826d42f40de70a1d01a8a9859631
Curtis King <curtis.alan.king@gmail.com>
2023-03-11 11:33:11 -0500

    test update

--- tenmo-server/src/test/java/com/techelevator/tenmo/dao/BaseDaoTests.java
+++ tenmo-server/src/test/java/com/techelevator/tenmo/dao/BaseDaoTests.java
@@ -21,4 +21,6 @@ public abstract class BaseDaoTests {
         dataSource.getConnection().rollback();
     }
 
+
+
 }
--- tenmo-server/src/test/java/com/techelevator/tenmo/tenmo/controllers/AccountControllerTest.java
+++ tenmo-server/src/test/java/com/techelevator/tenmo/tenmo/controllers/AccountControllerTest.java
@@ -1,28 +1,73 @@
 package com.techelevator.tenmo.tenmo.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.techelevator.tenmo.tenmo.controller.AccountController;
-import com.techelevator.tenmo.tenmo.services.AccountService;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@WebMvcTest(controllers = AccountController.class)
-@AutoConfigureMockMvc(addFilters = false)
-@ExtendWith(MockitoExtension.class)
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.techelevator.tenmo.tenmo.controller.AccountController;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.techelevator.tenmo.tenmo.model.Account;
+import com.techelevator.tenmo.tenmo.services.AccountService;
+
+import static org.junit.Assert.*;
+
 public class AccountControllerTest {
 
-	@Autowired
-	private MockMvcPrint mockMvc;
-	@MockBean
 	private AccountService accountService;
+	private AccountController accountController;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+	@Before
+	public void setup() {
+		accountService = mock(AccountService.class);
+		accountController = new AccountController(accountService);
+	}
 
+	@Test
+	public void getAccounts_returnsListOfAccounts() {
+		List<Account> accounts = new ArrayList<>();
+		accounts.add(new Account(1, 1, BigDecimal.valueOf(1000.0)));
+		accounts.add(new Account(2, 2, BigDecimal.valueOf(2000.0)));
+		when(accountService.getAccounts()).thenReturn(accounts);
 
+		List<Account> result = accountController.getAccounts();
+
+		assertEquals(2, result.size());
+		assertEquals(accounts.get(0), result.get(0));
+		assertEquals(accounts.get(1), result.get(1));
+	}
+
+	@Test
+	public void getAccount_returnsAccount() {
+		Account account = new Account(1, 1, BigDecimal.valueOf(1000.0));
+		when(accountService.getAccount(1)).thenReturn(account);
+
+		Account result = accountController.getAccount(1);
+
+		assertEquals(account, result);
+	}
+
+	@Test
+	public void saveAccount_returnsSavedAccount() {
+		Account account = new Account(1, 1, BigDecimal.valueOf(1000.0));
+		when(accountService.saveAccount(account)).thenReturn(account);
+
+		Account result = accountController.saveAccount(account);
+
+		assertEquals(account, result);
+	}
+
+	@Test
+	public void getAccountByAccountId_returnsAccount() {
+		Account account = new Account(1, 1, BigDecimal.valueOf(1000.0));
+		when(accountService.getAccountByAccountId(1)).thenReturn(account);
+
+		Account result = accountController.getAccountByAccountId(1);
+
+		assertEquals(account, result);
+	}
 }
